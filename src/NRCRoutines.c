e90b627308049a63f0498ce5a114314821d8acd1
DavisVaughan <davis@rstudio.com>
2021-08-03 14:38:28 -0400

    Comment out `exit()` call
    R CMD Check doesn't allow this. We will replace this with `R_alloc()` calls that will error automatically if they can't allocate.

--- src/NRCRoutines.c
+++ src/NRCRoutines.c
@@ -16,7 +16,7 @@ void nrerror(char error_text[])
         //fprintf(stderr,"Numerical Recipes run-time error...\n");
         //fprintf(stderr,"%s\n",error_text);
         //fprintf(stderr,"...now exiting to system...\n");
-        exit(1);
+        //exit(1);
 }
 
 float *vector(long nl, long nh)
