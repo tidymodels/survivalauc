824c5d1cd0f8e98551e8f4744f7c1ac5653c145e
DavisVaughan <davis@rstudio.com>
2021-08-03 14:37:31 -0400

    Comment out printing to `stderr`
    R CMD Check doesn't allow this

--- src/NRCRoutines.c
+++ src/NRCRoutines.c
@@ -13,9 +13,9 @@ int ludcmp_flag, flag;
 void nrerror(char error_text[])
 /* Numerical Recipes standard error handler */
 {
-        fprintf(stderr,"Numerical Recipes run-time error...\n");
-        fprintf(stderr,"%s\n",error_text);
-        fprintf(stderr,"...now exiting to system...\n");
+        //fprintf(stderr,"Numerical Recipes run-time error...\n");
+        //fprintf(stderr,"%s\n",error_text);
+        //fprintf(stderr,"...now exiting to system...\n");
         exit(1);
 }
 
