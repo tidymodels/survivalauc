e5c3e819e9749a6bfb83f1732dc087d6ce342f29
EmilHvitfeldt <emilhhvitfeldt@gmail.com>
2021-09-04 16:18:38 -0700

    document compute_auc_components

--- R/api.R
+++ R/api.R
@@ -1,6 +1,28 @@
+#' Compute Survival AUC
+#'
+#' Implementing the alternative estimators of the time-dependent area
+#' under the ROC curve, time-dependent sensitivity and specificity,
+#' and time-dependent ROC curves.
+#'
+#' @param time Numeric, survival/censoring time.
+#' @param status Numeric, censoring indicator taking value 1 if survival time is
+#'   observed and 0 otherwise.
+#' @param score Numeric, risk score obtained from a risk prediction model.
+#' @param threshold Numeric, thresholds used in estimating sensitivity and
+#' specificity.
+#'
+#' @details
+#' `time`, `status`, and `score` must have the same length.
+#'
+#' The corresponding estimators are presented in equations (9)-(11) in
+#' Chambless and Diao (Statistics in Medicine, 2006; 25: 3474-3486).
+#'
+#' @return NULL
+#' @export
 compute_auc_components <- function(time, status, score, threshold) {
 
   unique_time <- sort(unique(time))
 
-  .Call(survivalauc_compute_auc_components, time, status, score, threshold, unique_time)
+  .Call(survivalauc_compute_auc_components, time, status, score, threshold,
+        unique_time)
 }
